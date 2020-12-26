3e52202ecc28bc05ae9336d8bf82f9c6a22f406a
Timo Fischer <timo.fischer1308@gmail.com>
2020-12-26 17:18:08 +0100

    updated color sensor position method signatures

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/components/ConfigurationComponent.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/components/ConfigurationComponent.java
@@ -121,7 +121,7 @@ public class ConfigurationComponent extends Phrase<Void> {
         }
     }
 
-    public String getPosition(String block) {
+    public String getColourPosition(String block) {
         switch ( getProperty(block) ) {
             case SC.FRONT:
                 return SC.FRONT;
